9d5d4c8937fb3030a7492c93629b55e025b53259
Bala1324 <balachandiran1324@gmail.com>
2022-07-08 12:37:04 +0530

    imageUpload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [img, setImg] = useState([])
+  const handleChange = (event) => {
+    setImg(event.target.files[0]);
+    console.log(img)
+    // console.log(URL.createObjectURL(event.target.files[0]));
+  }
+
+  const submit = () => {
+    const formdata = new FormData();
+    formdata.append('file', img);
+    formdata.append('data', { name: 'bala' });
+    // console.log(img)
+
+    console.log("formData", formdata)
+    axios.post('http://localhost:5000/img-upload', formdata,
+      {
+        headers: { "Content-Type": "multipart/form-data" }
+      }).then((res) => {
+        console.log("res", res)
+      }).catch((err) => {
+        console.log(err)
+
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="file" className="form-control" name="upload_file" onChange={handleChange} />
+      <button type='buitton' onClick={submit}>UPLOAD</button>
+    </>
   );
 }
 
